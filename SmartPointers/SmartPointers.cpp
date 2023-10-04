5b9973a4e29cdb01ff94e57af9d15a8fc06a9dac
AndreyTokmakov <andtokm@yandex.ru>
2023-10-04 20:27:09 +0400

    04_10_2023__006

--- SmartPointers/SmartPointers.cpp
+++ SmartPointers/SmartPointers.cpp
@@ -13,10 +13,96 @@ Description : SmartPointers.cpp
 #include <string_view>
 #include <list>
 #include <memory>
+#include <utility>
 
 #include <boost/intrusive_ptr.hpp>
 #include <boost/smart_ptr/intrusive_ref_counter.hpp>
+#include <boost/smart_ptr/intrusive_ptr.hpp>
 
+
+namespace
+{
+    struct Long
+    {
+        long value {0};
+
+        explicit Long(int val = 0) : value {val} {
+            std::cout << "Long(" << value << ")\n";
+        }
+
+        Long(const Long &obj) {
+            this->value = obj.value;
+            std::cout << "Long(" << value << ") [Copy constructor]\n";
+        }
+
+        Long(Long &&obj) noexcept: value {std::exchange(obj.value, 0)} {
+            std::cout << "Long(" << value << ") [Move constructor]\n";
+        }
+
+        inline void setValue(long v) noexcept {
+            value = v;
+        }
+
+        [[nodiscard]]
+        inline long getValue() const noexcept {
+            return value;
+        }
+
+        ~Long() {
+            std::cout << "~Long(" << value << ")\n";
+        }
+
+        Long &operator=(const Long &right) {
+            std::cout << "Right: Long(" << right.value << ")\n";
+            std::cout << "[Copy assignment] (" << value << " -> " << right.value << ")" << std::endl;
+            if (&right != this)
+                value = right.value;
+            return *this;
+        }
+
+        Long &operator=(long val) {
+            std::cout << "[Copy assignment (from long)]" << std::endl;
+            this->value = val;
+            return *this;
+        }
+
+        Long &operator=(Long &&right) noexcept {
+            std::cout << "[Move assignment operator]" << std::endl;
+            if (this != &right) {
+                this->value = std::exchange(right.value, 0);
+            }
+            return *this;
+        }
+
+        Long &operator*(const Long &right) noexcept {
+            this->value *= right.value;
+            return *this;
+        }
+
+        /** Postfix increment: **/
+        Long operator++(int) {
+            auto prev = *this;
+            ++value;
+            return prev;
+        }
+
+        /** Prefix increment: **/
+        Long operator++() {
+            ++value;
+            return *this;
+        }
+
+        friend Long operator*(const Long &left, long v) noexcept {
+            return Long(left.value * v);
+        }
+
+        friend std::ostream &operator<<(std::ostream &stream, const Long &l) {
+            stream << l.value;
+            return stream;
+        }
+    };
+
+}
 
 namespace SmartPointers::Intrusive_Ref_Counter
 {
@@ -96,9 +182,39 @@ namespace SmartPointers::Intrusive_Ref_Counter
     }
 };
 
+
+namespace SmartPointers::Intrusive_Ptr
+{
+    struct LongWithCounter: Long
+    {
+        explicit LongWithCounter(long v): Long(v) {
+        }
+    };
+
+    void intrusive_ptr_add_ref([[maybe_unused]] LongWithCounter* ptr)
+    {
+        std::cout << "Called on creation. (" << ptr->getValue() << ")\n";
+        // p->AddRef();
+    }
+    void intrusive_ptr_release([[maybe_unused]] LongWithCounter* ptr)
+    {
+        std::cout << "Called on deletion. (" << ptr->getValue() << ")\n";
+        // p->Release();
+    }
+
+
+    void Test()
+    {
+        auto ptr = boost::intrusive_ptr<LongWithCounter>(new LongWithCounter(1));
+    }
+}
+
+
 void SmartPointers::TestAll()
 {
-    Intrusive_Ref_Counter::Test();
-    Intrusive_Ref_Counter::Test_SharedPtr();
+    // Intrusive_Ref_Counter::Test();
+    // Intrusive_Ref_Counter::Test_SharedPtr();
+
+    Intrusive_Ptr::Test();
 
 };
