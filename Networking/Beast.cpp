68ab8b6b524adedafc92f02b041ffa699e08ba85
Andrei Tokmakov <andtokm@yandex.ru>
2024-09-28 22:15:46 +0400

    28_09_2024_003

--- Networking/Beast.cpp
+++ Networking/Beast.cpp
@@ -148,7 +148,7 @@ namespace Beast::Client
     }
 }
 
-namespace  Beast::HTTP_Methods
+namespace Beast::HTTP_Methods
 {
     void do_get(beast::tcp_stream & stream,
                 http::request<http::string_body> & request,
@@ -257,6 +257,120 @@ namespace  Beast::HTTP_Methods
         // do_post(stream, request, buffer, response);
         do_delete(stream, request, buffer, response);
     }
+}
+
+namespace Beast::HTTP_Client_Async
+{
+    class session : public std::enable_shared_from_this<session>
+    {
+        tcp::resolver resolver;
+        beast::tcp_stream stream;
+        beast::flat_buffer buffer; // (Must persist between reads)
+        http::request<http::empty_body> request;
+        http::response<http::string_body> response;
+
+    public:
+        // Objects are constructed with a strand to ensure that handlers do not execute concurrently.
+        explicit session(net::io_context& ioc): resolver(net::make_strand(ioc)),
+                                                stream(net::make_strand(ioc)) {
+        }
+
+        // Start the asynchronous operation
+        void run(const std::string_view host,
+                 const std::string_view port,
+                 const std::string_view target,
+                 const int version)
+        {
+            // Set up an HTTP GET request message
+            request.version(version);
+            request.method(http::verb::get);
+            request.target(target);
+            request.set(http::field::host, host);
+            request.set(http::field::user_agent, BOOST_BEAST_VERSION_STRING);
+
+            // Look up the domain name
+            resolver.async_resolve(host,port,
+                                    beast::bind_front_handler(&session::on_resolve,shared_from_this()));
+        }
+
+        void on_resolve(const beast::error_code& ec,
+                        const tcp::resolver::results_type& results)
+        {
+            if (ec)
+                return fail(ec, "resolve");
+
+            // Set a timeout on the operation
+            stream.expires_after(std::chrono::seconds(30));
+
+            // Make the connection on the IP address we get from a lookup
+            stream.async_connect(results,
+                                  beast::bind_front_handler(&session::on_connect, shared_from_this()));
+        }
+
+        void on_connect(const beast::error_code& ec,
+                        const tcp::resolver::results_type::endpoint_type&)
+        {
+            if (ec)
+                return fail(ec, "connect");
+
+            // Set a timeout on the operation
+            stream.expires_after(std::chrono::seconds(30));
+
+            // Send the HTTP request to the remote host
+            http::async_write(stream, request,
+                               beast::bind_front_handler(&session::on_write,shared_from_this()));
+        }
+
+        void on_write(const beast::error_code& ec,
+                      std::size_t bytes_transferred)
+        {
+            boost::ignore_unused(bytes_transferred);
+            if (ec)
+                return fail(ec, "write");
+
+            // Receive the HTTP response
+            http::async_read(stream, buffer, response,
+                              beast::bind_front_handler(&session::on_read, shared_from_this()));
+        }
+
+        void on_read(beast::error_code ec,
+                     std::size_t bytes_transferred)
+        {
+            boost::ignore_unused(bytes_transferred);
+            if (ec)
+                return fail(ec, "read");
+
+            // Write the message to standard out
+            std::cout << response << std::endl;
+
+            // Gracefully close the socket
+            stream.socket().shutdown(tcp::socket::shutdown_both, ec);
+
+            // not_connected happens sometimes so don't bother reporting it.
+            if (ec && ec != beast::errc::not_connected)
+                return fail(ec, "shutdown");
+        }
+
+        void fail(beast::error_code ec, char const* what) {
+            std::cerr << what << ": " << ec.message() << "\n";
+        }
+    };
+
+    void Send_Request()
+    {
+        constexpr std::string_view host { "0.0.0.0"}, port { "52525"};
+        constexpr std::string_view path { "/index" };
+        constexpr int version { 11 };
+
+        // The io_context is required for all I/O
+        net::io_context ioc;
+
+        // Launch the asynchronous operation
+        std::make_shared<session>(ioc)->run(host, port, path, version);
+
+        // Run the I/O service. The call will return when the get operation is complete.
+        ioc.run();
+    }
 
 }
 
@@ -264,5 +378,7 @@ void Beast::TestAll([[maybe_unused]] const std::vector<std::string_view>& params
 {
     // Client::SimpleHttpGetRequest();
     // Client::SSL_Request_Test();
-    HTTP_Methods::Test();
+    // HTTP_Methods::Test();
+
+    HTTP_Client_Async::Send_Request();
 }
